b6fb9515c73cbc6cf533e78a46a377e837b621eb
Ben Life <77246839+benlife5@users.noreply.github.com>
2022-05-03 20:57:19 -0400

    csv importing into set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
+import {useState, useEffect} from "react"
+import Papa from "papaparse";
 
 function App() {
-  return (
-    <div className="App">
+  const [data, setData] = useState()
+  const [ingredients, setIngredients] = useState(() => new Set())
 
-    </div>
-  );
+  const loadIngredients = () => {
+    fetch("ingredients_list.csv")
+    .then(res => res.text())
+    .then(res => Papa.parse(res).data)
+    .then(data => {
+      const ingred = new Set()
+      for (let i = 1; i < data.length; i++) {
+        // console.log(data[i][1])
+        ingred.add(data[i][1].toLowerCase().trim().replaceAll(" ", ""))
+      }
+      // console.log(ingred)
+      setIngredients(() => ingred)
+    })
+  }
+
+  useEffect(() => {
+    loadIngredients()
+  }, []);
+  
+  console.log(ingredients)
+  console.log(ingredients.has("Farro".toLowerCase()))
+  if (ingredients.size > 0) {
+    return (
+      <div className="App">
+        {"Test"}
+        {ingredients}
+      </div>
+    );
+  } else {
+    return (<div className="App">Loading</div>)
+  }
 }
 
 export default App;
