456a8cadb607b9a6c7b6fc7c845c2eddc10e0624
Ben Life <77246839+benlife5@users.noreply.github.com>
2022-05-05 12:26:06 -0400

    input csv upload implemented

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Papa from "papaparse";
 function App() {
   const [data, setData] = useState()
   const [ingredients, setIngredients] = useState(() => new Set())
+  const [input, setInput] = useState()
 
   const loadIngredients = () => {
     fetch("ingredients_list.csv")
@@ -13,10 +14,8 @@ function App() {
     .then(data => {
       const ingred = new Set()
       for (let i = 1; i < data.length; i++) {
-        // console.log(data[i][1])
         ingred.add(data[i][1].toLowerCase().trim().replaceAll(" ", ""))
       }
-      // console.log(ingred)
       setIngredients(() => ingred)
     })
   }
@@ -24,14 +23,45 @@ function App() {
   useEffect(() => {
     loadIngredients()
   }, []);
+
+  const upload = async (e) => {
+    e.preventDefault()
+    Papa.parse(e.target[0].files[0], {
+      header: true,
+      skipEmptyLines: "greedy",
+      complete: function (results) {
+        for (let i = 0; i < results.data.length; i++) {
+          if (results.data[i]["Menu Title"] === "" && i > 0) {
+            results.data[i]["Menu Title"] = results.data[i - 1]["Menu Title"]
+          }
+          if (results.data[i].Partner === "" && i > 0) {
+            results.data[i].Partner = results.data[i - 1].Partner
+          }
+          if (results.data[i].Meal === "" && i > 0) {
+            results.data[i].Meal = results.data[i - 1].Meal
+          }
+          results.data[i].Ingredients = Papa.parse(results.data[i].Ingredients).data[0]
+          results.data[i].normalizedIngredients = []
+          results.data[i].Ingredients.forEach(
+            (ingredient) => results.data[i].normalizedIngredients.push(ingredient.toLowerCase().trim().replaceAll(" ", ""))
+          )
+        }
+        
+        console.log(results.data)
+        setInput(results.data)
+      },
+      error: function () {alert("Invalid file")}
+    });
+  }
   
   console.log(ingredients)
-  console.log(ingredients.has("Farro".toLowerCase()))
   if (ingredients.size > 0) {
     return (
       <div className="App">
-        {"Test"}
-        {ingredients}
+        <form onSubmit={(e) => upload(e)}>
+          <input type="file" label="Upload file" />
+          <input type="Submit" label="Upload"/>
+        </form>
       </div>
     );
   } else {
